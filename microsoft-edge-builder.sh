5b85debf8f6d10653144d6b990ee9538fdd186f1
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-01-09 02:01:02 +0100

    Update microsoft-edge-builder.sh
    fix https://github.com/ivan-hc/MS-Edge-appimage/issues/6

--- microsoft-edge-builder.sh
+++ microsoft-edge-builder.sh
@@ -8,7 +8,7 @@ cd ./tmp || exit 1
 
 # DOWNLOAD APPIMAGETOOL
 if ! test -f ./appimagetool; then
-	wget -q "$(wget -q https://api.github.com/repos/probonopd/go-appimage/releases -O - | sed 's/"/ /g; s/ /\n/g' | grep -o 'https.*continuous.*tool.*86_64.*mage$')" -O appimagetool
+	wget -q https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage -O appimagetool
 	chmod a+x ./appimagetool
 fi
 
@@ -34,27 +34,28 @@ _create_edge_appimage(){
 	fi
 	tar xf ./control.tar.xz
 	VERSION=$(cat control | grep Version | cut -c 10-)
-	ARCH=x86_64 VERSION=$(./appimagetool -v | grep -o '[[:digit:]]*') ./appimagetool -s ./"$APP".AppDir
-	mv ./*.AppImage ./Microsoft-Edge-"$CHANNEL"-"$VERSION"-x86_64.AppImage || exit 1
+	ARCH=x86_64 ./appimagetool --comp zstd --mksquashfs-opt -Xcompression-level --mksquashfs-opt 20 \
+	-u "gh-releases-zsync|$GITHUB_REPOSITORY_OWNER|MS-Edge-appimage|continuous|*x86_64.AppImage.zsync" \
+	./"$APP".AppDir Microsoft-Edge-"$CHANNEL"-"$VERSION"-x86_64.AppImage || exit 1
 }
 
 CHANNEL="stable"
 mkdir -p "$CHANNEL" && cp ./appimagetool ./"$CHANNEL"/appimagetool && cd "$CHANNEL" || exit 1
 _create_edge_appimage
 cd ..
-mv ./"$CHANNEL"/*.AppImage ./
+mv ./"$CHANNEL"/*.AppImage* ./
 
 CHANNEL="beta"
 mkdir -p "$CHANNEL" && cp ./appimagetool ./"$CHANNEL"/appimagetool && cd "$CHANNEL" || exit 1
 _create_edge_appimage
 cd ..
-mv ./"$CHANNEL"/*.AppImage ./
+mv ./"$CHANNEL"/*.AppImage* ./
 
 CHANNEL="dev"
 mkdir -p "$CHANNEL" && cp ./appimagetool ./"$CHANNEL"/appimagetool && cd "$CHANNEL" || exit 1
 _create_edge_appimage
 cd ..
-mv ./"$CHANNEL"/*.AppImage ./
+mv ./"$CHANNEL"/*.AppImage* ./
 
 cd ..
 mv ./tmp/*.AppImage ./
